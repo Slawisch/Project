e9b92e7d15eb77f98c885922a48087b91f4d5a3f
bo-shev <56201331+bo-shev@users.noreply.github.com>
2020-09-21 00:32:13 +0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,20 +1,18 @@
 var scroll = 126;
 
-jQuery(function($)
+if (document.documentElement.clientWidth >= 780)
 {
-  $(window).scroll(function()
-  {
-    if($(this).scrollTop()>scroll)
-    {
-      $('#buttons').addClass('fixed');
-      $('#header-line').addClass('fix-line');
-      $('#mybgbutton').addClass('fix-mybuttonfon');
-    }
-    else if ($(this).scrollTop()<scroll)
-    {
-      $('#buttons').removeClass('fixed');
-      $('#header-line').removeClass('fix-line');
-      $('#mybgbutton').removeClass('fix-mybuttonfon');
-    }
+  jQuery(function ($) {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > scroll) {
+        $('#buttons').addClass('fixed');
+        $('#header-line').addClass('fix-line');
+        $('#mybgbutton').addClass('fix-mybuttonfon');
+      } else if ($(this).scrollTop() < scroll) {
+        $('#buttons').removeClass('fixed');
+        $('#header-line').removeClass('fix-line');
+        $('#mybgbutton').removeClass('fix-mybuttonfon');
+      }
+    });
   });
-});
+}
